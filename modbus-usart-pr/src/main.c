2c79f91813064fe7e6b64728991b98b2f6fd2fb9
valmit <sliper98@gmail.com>
2019-05-29 21:32:31 +0700

    looked at project a little bit

--- modbus-usart-pr/src/main.c
+++ modbus-usart-pr/src/main.c
@@ -3,26 +3,67 @@
 
 //legs for modbus: PD2 - RTS, RXD - PC11, TXD - PC10
 
+
+char usart_data_recieved = 0;
+
 void init_usart() {
-	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;	//USART1 Clock ON
-	USART1->CR1  |= USART_CR1_UE | USART_CR1_TE | USART_CR1_RE; // USART1 ON, TX ON, RX
+	RCC->APB1ENR |= RCC_APB1ENR_USART3EN;	//USART3 Clock ON
+
+    RCC->AHBENR |= RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIODEN |
+    				RCC_AHBENR_GPIOCEN;
+
+    GPIOD->MODER |= GPIO_MODER_MODER2;
+    GPIOC->MODER |= GPIO_MODER_MODER11 | GPIO_MODER_MODER10;
+
+//	RCC->APB2ENR  	|= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN;
+
+	USART3->CR1  |= USART_CR1_UE | USART_CR1_TE | USART_CR1_RE; // USART1 ON, TX ON, RX
 
 
-	USART1->CR1 |= USART_CR1_RTOIE;
-	USART1->CR2 |= USART_CR2_RTOEN;
-	USART1->CR3 |= USART_CR3_DEM; // RS 485 driver enable
-	USART1->BRR 	= 0xEA6; // set some baudrate
-	USART1->RTOR = 42304; // modbus RTU timeout placeholder
-    RCC->AHBENR |= RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOAEN;
+	USART3->CR1 |= USART_CR1_RTOIE;
+	USART3->CR2 |= USART_CR2_RTOEN;
+	USART3->CR3 |= USART_CR3_DEM; // RS 485 driver enable
+	USART3->BRR 	= 0xEA6; // set some baudrate
+	USART3->RTOR = 42304; // modbus RTU timeout placeholder
+	// GPIOA Clock ON. Alter function clock ON
 
-	RCC->APB2ENR  	|= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN; 	// GPIOA Clock ON. Alter function clock ON
+	NVIC_EnableIRQ (USART3_4_IRQn); // i don't understand what 4 means
+
 
 
 }
 
-int main(void)
-{
-	while(1 == 1) {
 
+void USART3_4_IRQHandler() {
+
+	usart_data_recieved = 1;
+	USART3->ICR = USART_ICR_RTOCF; //clear interrupt request
+}
+
+
+void transmit_data (uint16_t data) {
+	if ((USART3->ISR & USART_ISR_TXE) == USART_ISR_TXE) {
+		USART3->TDR = data;
+	}
+}
+
+void slave_message_act(uint16_t message) {
+
+}
+
+void master_message_act(uint16_t message) {
+
+}
+
+
+int main(void){
+
+uint16_t recieved = 0;
+
+	while(1 == 1) {
+		if (usart_data_recieved == 1) {
+			//TODO do something with recieved data
+			recieved = USART3->RDR;
+		}
 	}
 }
