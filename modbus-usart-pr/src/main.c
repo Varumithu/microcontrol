7f8d077cae2ad96f72dc750dda0ba145b1543ef0
valmit <sliper98@gmail.com>
2019-05-29 19:10:46 +0700

    fff

--- modbus-usart-pr/src/main.c
+++ modbus-usart-pr/src/main.c
@@ -6,13 +6,13 @@
 void init_usart() {
 	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;	//USART1 Clock ON
 	USART1->CR1  |= USART_CR1_UE | USART_CR1_TE | USART_CR1_RE; // USART1 ON, TX ON, RX
-	USART1->CR2 |= USART_CR2_ABREN; // abrmode = 0
+
 
 	USART1->CR1 |= USART_CR1_RTOIE;
 	USART1->CR2 |= USART_CR2_RTOEN;
 	USART1->CR3 |= USART_CR3_DEM; // RS 485 driver enable
 	USART1->BRR 	= 0xEA6; // set some baudrate
-
+	USART1->RTOR = 42304; // modbus RTU timeout placeholder
     RCC->AHBENR |= RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOAEN;
 
 	RCC->APB2ENR  	|= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN; 	// GPIOA Clock ON. Alter function clock ON
